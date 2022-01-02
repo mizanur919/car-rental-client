9547a47152032221aaf72866b02cc4511ad48fc6
mizan <mizanur919@gmail.com>
2022-01-02 21:48:03 +0600

    current changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
-<<<<<<< HEAD
 import Home from "./Components/Home/Home/Home";
-=======
-import Choose from "./Components/Home/Choose/Choose";
-import SingleBrand from "./Components/Home/SingleBrand/SingleBrand";
-import Navigation from "./Components/Shared/Navigation/Navigation";
->>>>>>> 6bc86fcdda882c750e8d32459a581b03f73ac6c4
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Home />
-=======
-      <Navigation />
-      <SingleBrand />
-      <Choose />
->>>>>>> 6bc86fcdda882c750e8d32459a581b03f73ac6c4
     </div>
   );
 }
