dd7af57321451c3697de0af3f071363712b93d23
monzu25 <monzu.ctg25@gmail.com>
2022-01-02 20:29:53 +0600

    This is monzu

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -4,38 +4,18 @@ import { Slide } from "react-slideshow-image";
 import bg from "../../../images/bg1.jpg";
 import "./Banner.css";
 import { Carousel } from "react-bootstrap";
+import BackgroundSlider from "react-background-slider";
 
 const bg1 = bg;
 const slideImages = [bg1, bg1, bg1];
 
 const Banner = () => {
   return (
-    <div>
-      <Carousel>
-        <Carousel.Item interval={1000}>
-          <img className="d-block w-100" src={bg1} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img className="d-block w-100" src={bg1} alt="Second slide" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={bg1} alt="Third slide" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <div className="justify-content-start">
+      <div>
+        <h1>This is Slider Sample</h1>
+      </div>
+      <BackgroundSlider images={[bg1, bg1, bg1]} duration={8} transition={2} />
     </div>
   );
 };
