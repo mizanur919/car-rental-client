125eaa6d94bd4eb4f06fa8201762ad0d559112ea
alamgirjuab <alamgirjuab@gmail.com>
2022-01-02 20:26:00 +0600

    brand updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Brand from "./Components/Home/Brand/Brand";
+import SingleBrand from "./Components/Home/SingleBrand/SingleBrand";
 import Navigation from "./Components/Shared/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <SingleBrand></SingleBrand>
     </div>
   );
 }
