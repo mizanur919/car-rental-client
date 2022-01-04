e42bbe28643aacb8e1050b6f35643003458d57e7
mizan <mizanur919@gmail.com>
2022-01-04 22:31:44 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
-<<<<<<< HEAD
 import Home from "./Components/Home/Home/Home";
 import AddBrand from "./Components/Dashboard/AddBrand/AddBrand";
 import NotFound from "./Components/Shared/NotFound/NotFound";
 import AddServices from "./Components/Dashboard/AddServices/AddServices";
-=======
-import About from "./Components/AboutUs/About";
-import Choose from "./Components/Home/Choose/Choose";
-import ContactUs from "./Components/Home/ContactUs/ContactUs";
-import Services from "./Components/Home/Services/Services";
->>>>>>> alamgir
 import SingleBrand from "./Components/Home/SingleBrand/SingleBrand";
 import AuthProvider from "./Context/AuthProvider";
 import Login from "../src/Components/Shared/Login/Login";
